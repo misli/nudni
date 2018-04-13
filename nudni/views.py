c7d9245ddece48dd500ff08486e5b7f80f064c40
Jakub Dorňák <jakub.dornak@misli.cz>
2018-04-13 09:37:11 +0200

    use get_or_create instead of hardcoding an id

--- nudni/views.py
+++ nudni/views.py
@@ -13,8 +13,8 @@ class Home(views.View):
     template_name = 'nudni/index.html'
 
     def get(self, request):
-        lenka = models.Artist.objects.get(pk=1)
-        ondrej = models.Artist.objects.get(pk=2)
+        lenka = models.Artist.objects.get_or_create(user=User.objects.get_or_create(username='lenka')[0])[0]
+        ondrej = models.Artist.objects.get_or_create(user=User.objects.get_or_create(username='ondrej')[0])[0]
         work_list = models.Work.objects.all()
         return render(request, self.template_name, {
             'lenka': lenka,
